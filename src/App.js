387a133109a2a199a7bd9f2695beefba4ecfa685
sean-udayantha <seanudayantha@mail.com>
2023-04-19 10:54:24 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          deploy test!
+          update deploy test!
         </p>
       </header>
     </div>
