aad5cabfd26372e2b71733a79fb6cee54bf7ba9a
sean-udayantha <seanudayantha@mail.com>
2023-04-19 12:43:51 +0530

    deploy test 2

--- src/App.js
+++ src/App.js
@@ -1,14 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
-
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
 function App() {
+
+  const [employee, setEmployee] = useState('');
+  console.log("🚀 ~ file: App.js:8 ~ App ~ employee:", employee)
+
+  useEffect(() => {
+
+    const getAllEmployee = async () => {
+        await axios.get(`https://dummy.restapiexample.com/api/v1/employees`).then((res) => {
+          setEmployee(res.data.data);
+        }).catch((err) => {
+            console.log(err.massage);
+        })
+    }
+    getAllEmployee(); 
+}, [])
+  
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          update deploy test!
-        </p>
+
+        {employee.map((employee,index)=>(
+             <p>
+              {employee.employee_name}
+             </p>
+
+
+        ))}
+       
+
+             <p>
+                update deploy test!
+             </p>
       </header>
     </div>
   );
